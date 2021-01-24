d3556672997f9300ce1d12202cb5a7407a076708
Omri Shapira <omri@redefinemeat.com>
2021-01-24 15:32:39 +0200

    adding missing files

---
+++ src/main/java/il/ac/afeka/wishlistservice/WishlistserviceApplication.java
@@ -0,0 +1,13 @@
+package il.ac.afeka.wishlistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlistserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishlistserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/il/ac/afeka/wishlistservice/WishlistserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package il.ac.afeka.wishlistservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
